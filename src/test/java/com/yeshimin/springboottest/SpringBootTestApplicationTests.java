85feb93d4fd509c4d527408e58dbd86d6dad936e
yeshimin <ysm@yeshimin.com>
2023-10-26 01:10:55 +0800

    init

---
+++ src/test/java/com/yeshimin/springboottest/SpringBootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeshimin.springboottest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
